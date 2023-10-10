8eae013ab8078a13a66661957dec747de900d592
Kamil Sobol <sobkamil@amazon.com>
2023-10-09 19:41:05 -0700

    s3 deployment hotswap with more memory

--- testapps/testapp001/lib/testapp001-stack.ts
+++ testapps/testapp001/lib/testapp001-stack.ts
@@ -32,6 +32,7 @@ export class Testapp001Stack extends cdk.Stack {
     new s3deploy.BucketDeployment(this, 'DeployFiles', {
       sources: [s3deploy.Source.asset('./some_files')],
       destinationBucket: bucket,
+      memoryLimit: 512,
     });
 
     new lambda.Function(this, 'lambda1', {
